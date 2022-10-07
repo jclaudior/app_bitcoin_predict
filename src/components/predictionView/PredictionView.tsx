da4c73bd0adac5e68fac7dc6a3915db4213bb311
“Yjclaudior <>
2022-10-06 23:22:50 -0300

    ajusted prediction view

--- src/components/predictionView/PredictionView.tsx
+++ src/components/predictionView/PredictionView.tsx
@@ -4,12 +4,16 @@ import bitcoin_up from '../../assets/bitcoin_up.png';
 import bitcoin_down from '../../assets/bitcoin_down.png';
 import {useLocation } from 'react-router-dom'
 import { Container } from './Styles';
+import moment from 'moment';
 
 function PredictionView(props: BitcoinPredictType){
+    moment.locale('pt-BR')
+    let now = moment().format("YYYYMMDD")
+    let predictDate = moment(props.body?.date).format("YYYYMMDD")
     let divClose =  <span><b>CLOSE:</b> {props.body?.simpleBitcoin.close}</span>;
     return (
         <Container>
-            <span><b>PREDICTION:</b> {props.body?.prediction} {useLocation().pathname == '/'? null : divClose } </span>
+            <span><b>PREDICTION:</b> {props.body?.prediction} {useLocation().pathname == '/byparameters' || now == predictDate ? null : divClose } </span>
             <img src={props.body?.ascend ? bitcoin_up : bitcoin_down }/>
         </Container>
     )
